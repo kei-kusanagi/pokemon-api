05d07b4df28753ac53398dec511520e68151cc8a
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-16 17:53:14 -0600

    muestro imagen mewtwo en ves de hola mundo

--- busqueda/views.py
+++ busqueda/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(*args, **kwars):
-    return HttpResponse("<h1>Hola mundo</1>")
+    return HttpResponse("<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png' alt='MDN'>")
