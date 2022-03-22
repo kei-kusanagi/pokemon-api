c4273ac7c94feb7565e3be8d164d8a00486f5eae
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-22 14:24:59 -0600

    casi me rindo y nomas no queda esta cosa :(

--- busqueda/views.py
+++ busqueda/views.py
@@ -7,9 +7,34 @@ import json
 
 # Create your views here.
 def index(request):
-    queryset = {}
-    queryset = {'name': request.GET.get }
-    return render(request, 'index.html', queryset)
+    queryset = request.GET.get('busqueda')
+    if queryset:
+        url = 'https://pokeapi.co/api/v2/pokemon/'
+        nombre=queryset
+        
+        peticion = request.get(url + str(nombre))
+        respuesta = json.loads(peticion.content)
+
+        pokemon_data = {
+        'name': '',
+        
+        'abilities': '',
+        
+        'sprites': ''
+        }
+        
+        pokemon_data['name'] = respuesta['name']
+        pokemon_data['abilities'] = respuesta['abilities']
+        pokemon_data['sprites'] = respuesta['sprites']['other']['official-artwork']['front_default']
+        poke_imagen = pokemon_data['sprites']
+        imagen = {'imagen': poke_imagen}
+        return render(request, 'index.html')
+    
+        
+    else:
+        print("no encontrado)")
+
+    return render(request, 'index.html')
 
 
 
@@ -17,6 +42,7 @@ def index(request):
 def buscarPokemon(request):
     
     url = 'https://pokeapi.co/api/v2/pokemon/'
+    
     nombre=request.POST
     
     peticion = request.get(url + str(nombre))
