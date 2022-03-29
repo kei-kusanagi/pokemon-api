9a1b87572ee080f212523f9ce386188528642b73
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-03-28 19:12:06 -0600

    el problema era el json, tenia que usar diferente al de django

--- busqueda/views.py
+++ busqueda/views.py
@@ -1,40 +1,63 @@
 from urllib import request
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
+from pprint import pprint
 import json
+import requests
 
 
 # Create your views here.
 def index(request):
     queryset = request.GET.get('busqueda')
+    print("request")
+    pprint(request)
+    print("request.GET")
+    pprint(request.GET)
     if queryset:
         url = 'https://pokeapi.co/api/v2/pokemon/'
-        nombre=queryset
-        
-        peticion = request.get(url + str(nombre))
-        respuesta = json.loads(peticion.content)
-
-        pokemon_data = {
-        'name': '',
-        
-        'abilities': '',
-        
-        'sprites': ''
-        }
-        
-        pokemon_data['name'] = respuesta['name']
-        pokemon_data['abilities'] = respuesta['abilities']
-        pokemon_data['sprites'] = respuesta['sprites']['other']['official-artwork']['front_default']
-        poke_imagen = pokemon_data['sprites']
-        imagen = {'imagen': poke_imagen}
-        return render(request, 'index.html')
+        nombre=str(queryset)
+        link = url + nombre
+        peticion = requests.get(link)
+        print("peticion imagen de del poquemon buscado")
+        pprint(peticion)
+        if peticion.status_code == 200:
+            print("peticion")
+            pprint(peticion)
+            respuesta = peticion.json()
+            # print("respuesta")
+            # pprint(respuesta)
+            pokemon_data = {
+            'name': '',
+            
+            'abilities': '',
+            
+            'sprites': ''
+            }
+            
+            pokemon_data['name'] = respuesta['name']
+            pokemon_data['abilities'] = respuesta['abilities']
+            pokemon_data['sprites'] = respuesta['sprites']['other']['official-artwork']['front_default']
+            poke_imagen = pokemon_data['sprites']
+            imagen = {'imagen': poke_imagen}
+            print("imagen")
+            pprint(imagen)
+            
+            return render(request, 'index.html', imagen)
+        else:
+            print("no encontro el pokemon")
+            poke_imagen="https://p4.wallpaperbetter.com/wallpaper/732/308/18/art-pokemon-illustration-minimalism-pikachu-hd-wallpaper-preview.jpg"
+            imagen = {'imagen': poke_imagen}
+            return render(request, 'index.html', imagen)
     
         
     else:
-        print("no encontrado)")
+        print("imagen de pikachu y eevee")
+        poke_imagen="https://gaminguardian.com/wp-content/uploads/2018/03/DY-O7RqWsAApCSb.jpg"
+        print("no encontrado")
+        imagen = {'imagen': poke_imagen}
+        return render(request, 'index.html', imagen)
 
-    return render(request, 'index.html')
+    
 
 
 
